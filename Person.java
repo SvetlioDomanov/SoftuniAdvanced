c9166717411bc360295466f47332def36015cf63
Svetoslav Domanov <92541703+SvetlioDomanov@users.noreply.github.com>
2021-10-23 11:36:45 +0300

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,52 @@
+package hotel;
+
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+    private String hometown;
+
+    public Person(String name, int id, int age, String hometown) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    @Override
+    public String toString() {
+        return "Person " + this.name + ": " + this.id + ", Age: " + this.age + ", Hometown: " + this.hometown;
+    }
+}
